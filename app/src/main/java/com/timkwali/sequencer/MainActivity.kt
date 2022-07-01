103a29e2e1e75e3feb27269c6f9a6f728865117b
timkwali <timkwalit@accessbankplc.com>
2022-07-01 16:51:41 +0100

    set screen orientation

--- app/src/main/java/com/timkwali/sequencer/MainActivity.kt
+++ app/src/main/java/com/timkwali/sequencer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.timkwali.sequencer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        play()
+    }
+
+    private fun play() {
+        val  music = MediaPlayer.create(this, R.raw.a1)
+        music.start();
     }
 }
